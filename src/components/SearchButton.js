d29b4cf63ea0ce70a04b9386d821d83bc05d1bdb
Rich Hauck <rich@hauckinteractive.com>
2020-08-16 21:21:06 -0400

    Added click handler for search button

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -3,10 +3,10 @@ import classNames from 'classnames';
 import searchIcon from '../images/search-24px.svg';
 
 function SearchButton(props){
-    const onClickHandler = (e) => {
-        props.onClick(e);
-    }
     const classes = classNames('searchbutton', 'button');
+    const onClickHandler = (e) => {
+        if(props.onClick) props.onClick(e);
+    }
     return <button onClick={onClickHandler} className={classes}><div><img src={searchIcon} alt="Search"/> <span>Search</span></div></button>
 }
 export default SearchButton;
