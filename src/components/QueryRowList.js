6b058a8a7f5d3cfcad18f4681f93dc7d5f7eca83
Rich Hauck <rich@hauckinteractive.com>
2020-08-23 15:25:09 -0400

    Added CSS transition

--- src/components/QueryRowList.js
+++ src/components/QueryRowList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useObserver} from 'mobx-react-lite';
 import {StoreContext} from '../stores/QueryProvider';
 import QueryRow from './QueryRow';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 
 const QueryRowList = () => {
     const store = React.useContext(StoreContext);
@@ -11,13 +12,20 @@ const QueryRowList = () => {
     }
   
     return useObserver(() => (
-      <div className="queryrowlist">
+      <TransitionGroup className="queryrowlist">
+
         {store.queries.map((query, id) => (
+          <CSSTransition
+              key={id}
+              timeout={700}
+              classNames="fade"
+            >
           <QueryRow data-id={id} key={id}>{query}</QueryRow>
+          </CSSTransition>
         ))}
 
         <button className="button" onClick={addRow}>And</button>
-      </div>
+      </TransitionGroup>
       )
     )
   }
